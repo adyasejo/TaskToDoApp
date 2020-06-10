21dca18eafabfdbfd0073356da526c68404a597e
Joel ADIBOLO <adyasejo@yahoo.com>
2020-06-09 22:30:18 -0400

    create back-end scaffold

---
+++ src/main/java/local/adyasejo/app/TaskToDoApplication.java
@@ -0,0 +1,13 @@
+package local.adyasejo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/adyasejo/app/TaskToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package local.adyasejo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
